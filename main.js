420fa3a833f76438a9f0aca3f2c0dbaf25377884
Björn Gustafsson <bjorn.gustafsson@tretton37.com>
2016-04-25 12:28:24 +0200

    Fix stuffs...

--- main.js
+++ main.js
@@ -2,23 +2,32 @@ var starfield = document.getElementById('starfield'),
 	nebula = document.getElementById('nebula'),
 	pig = document.getElementById('pig'),
 	pigX = -530,
-	pigY = 100,
+	pigY = window.innerHeight / 2,
+	pigYoffset = 0,
 	nebulaX = 0,
 	nebulaY = 0,
 	counter = 0;
 
 render = function() {
 	pigX = pigX + 0.5;
-	pigY = 100 + (50 * Math.sin(counter / 100));
+	pigYoffset = (window.innerHeight / 8) * Math.sin(counter / 100);
 
 	counter++;
 	pig.style.right = pigX + 'px';
-	pig.style.top = pigY + 'px';
+	pig.style.top = (pigY + pigYoffset) + 'px';
 	pig.style.transform = 'rotate(' + ((counter / 10) % 360) + 'deg)';
 
 	nebula.style.transform = 'rotate(-' + ((counter / 30) % 360) + 'deg) scale(' + (2 + Math.sin(counter / 1000) % 360) + ')';
 
+	if (pigX > window.innerWidth + 530) {
+		pigX = -530;
+	}
+
 	window.requestAnimationFrame(render);
+};
+
+window.onresize = function() {
+	pigY = window.innerHeight / 2;
 };
 
 render();
